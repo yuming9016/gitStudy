286477cd0e89636c6c63daaa9cb4f5fc7e765e75
yuming9016 <75942127+yuming9016@users.noreply.github.com>
2023-04-02 20:34:12 +0800

    Add files via upload

---
+++ gitHubStudy/src/main/java/com/githubstudy/githubstudy/GitHubStudyApplication.java
@@ -0,0 +1,13 @@
+package com.githubstudy.githubstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubStudyApplication.class, args);
+    }
+
+}
---
+++ gitHubStudy/src/test/java/com/githubstudy/githubstudy/GitHubStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubstudy.githubstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
